4e1ae2bd2a2b81f0a4f06fc50383b649da625278
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2024-01-09 19:50:17 +0200

    Ex2: Level 1, 2

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,13 +1,22 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = new Array(this.height);
+    for (let i = 0; i < this.height; i++) {
+      this.board[i] = new Array(this.width).fill(".");
+    }
+  }
+
+  drop(shape) {
+    this.board[0][Math.floor(this.width / 2)] = shape;
   }
 
   toString() {
-    return "...\n...\n...\n";
+    return this.board.map(row => row.join("")).join("\n") + '\n';
   }
 }
