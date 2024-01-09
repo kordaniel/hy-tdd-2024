f3f5a30def339a650ed1be965791a2c39200e314
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2024-01-09 22:07:56 +0200

    Ex2: Level 3, 5

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -2,6 +2,7 @@ import { RotatingShape } from "./RotatingShape.mjs"
 
 export class Tetromino {
 
+  static I_SHAPE = new RotatingShape(".....\n.....\nIIII.\n.....\n.....");
   static T_SHAPE = new RotatingShape(".T.\nTTT\n...");
 
 }
