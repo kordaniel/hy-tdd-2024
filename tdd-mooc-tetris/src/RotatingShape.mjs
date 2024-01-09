df6398e93cb50be02888778b8c8a70bdcc1dc524
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2024-01-09 21:17:39 +0200

    Ex2: Level 2, 2

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -1,7 +1,16 @@
 export class RotatingShape {
 
   constructor(shape) {
-    this.shape = shape.split('\n').map(l => l.trim().split());
+    this.shape = shape.split("\n").map(l => l.trim().split(""));
+  }
+
+  rotateRight() {
+    return new RotatingShape(
+      this.shape[0]
+        .map((_, i) => this.shape.map(row => row[i]).reverse())
+        .map(row => row.join(""))
+        .join("\n")
+    );
   }
 
   toString() {
