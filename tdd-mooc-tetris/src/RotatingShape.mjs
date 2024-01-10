83f18bb054a097eace9e02727462deaa9846fb2c
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2024-01-10 11:48:16 +0200

    Ex2, Level 3, 7

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -1,25 +1,28 @@
 export class RotatingShape {
 
-  constructor(shape) {
-    this.shape = shape.split("\n").map(l => l.trim().split(""));
+  constructor(shape, rotations = 4, rotation = 0) {
+    this.shape = Array.isArray(shape) ? shape : shape.split("\n").map(l => l.trim().split(""));
+    this.rotations = rotations;
+    this.rotation = rotation;
   }
 
   rotateRight() {
     return new RotatingShape(
-      this.shape[0]
-        .map((_, i) => this.shape.map(row => row[i]).reverse())
-        .map(row => row.join(""))
-        .join("\n")
+      this.shape[0].map((_, i) => this.shape.map(row => row[i]).reverse()),
+      this.rotations,
+      this.rotation + 1 % this.rotations
     );
   }
 
   rotateLeft() {
-    return new RotatingShape(
-      this.shape[0]
-        .map((_, i) => this.shape.map(row => row[row.length - 1 - i]))
-        .map(row => row.join(""))
-        .join("\n")
-    )
+    if (this.rotation > 0) {
+      return new RotatingShape(
+        this.shape[0].map((_, i) => this.shape.map(row => row[row.length - 1 - i])),
+          this.rotations,
+          this.rotation - 1
+        );
+    }
+    return Array.from(Array(this.rotations-1)).reduce((acc, _) => acc.rotateRight(), this);
   }
 
   toString() {
--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -2,7 +2,7 @@ import { RotatingShape } from "./RotatingShape.mjs"
 
 export class Tetromino {
 
-  static I_SHAPE = new RotatingShape(".....\n.....\nIIII.\n.....\n.....");
+  static I_SHAPE = new RotatingShape(".....\n.....\nIIII.\n.....\n.....", 2);
   static T_SHAPE = new RotatingShape(".T.\nTTT\n...");
 
 }
