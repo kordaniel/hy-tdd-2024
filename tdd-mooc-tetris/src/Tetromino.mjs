d35a7a5fa43982db55e80414b198f9f62680b226
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2024-01-12 11:39:05 +0200

    Ex2, Level 5, Add missing semicolon

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -3,7 +3,7 @@ import { RotatingShape } from "./RotatingShape.mjs"
 export class Tetromino {
 
   static I_SHAPE = new RotatingShape(".....\n.....\nIIII.\n.....\n.....", 2);
-  static O_SHAPE = new RotatingShape(".OO\n.OO\n...", 1)
+  static O_SHAPE = new RotatingShape(".OO\n.OO\n...", 1);
   static T_SHAPE = new RotatingShape(".T.\nTTT\n...", 4);
 
 }
